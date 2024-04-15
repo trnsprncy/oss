36de3a9ec25ac58439deaa5f56daac7b368c1836
trnsprncy <transparencyui@gmail.com>
2024-04-14 21:26:26 -0400

    adds named utils, hooks imports

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
 import CookieConsentProvider from "./context/consent-provider";
+export * as utils from "./utils";
+export * as hooks from "./hooks";
 export default CookieConsentProvider;
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 import CookieConsentProvider from "./context/consent-provider";
+export * as utils from "./utils";
+export * as hooks from "./hooks";
 
 export default CookieConsentProvider;
